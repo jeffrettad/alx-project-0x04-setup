34e07cedc1b4fe422baddca1f42368b597d83411
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-13 03:32:41 -0700

    .

---
+++ alx-project-0x06/interface/index.tsx
@@ -0,0 +1,22 @@
+// interface/index.ts
+
+// For Button.tsx
+["import { PageRouteProps }"]
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
+  action?: () => void;
+}
+
+// For Layout.tsx
+import { ReactNode } from "react";
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+// For pages/index.tsx (Home)
+export interface PageRouteProps {
+  pageRoute: string;
+}
---
+++ alx-project-0x06/pages/text-to-image.tsx
@@ -0,0 +1,3 @@
+export default function TextToImage() {
+  return <div className="p-8">Text to Image Page</div>;
+}
