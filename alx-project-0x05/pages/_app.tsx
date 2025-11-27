1c131e5a73e775f69d4c458c7a62f025d54cb8f0
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-11-27 03:51:41 -0800

    Update _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/layouts/Layout";
+kimport Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
