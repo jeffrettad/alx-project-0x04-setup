119e95935fd3fb30474de1cb6c01097f7a5310c7
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-11-27 03:20:44 -0800

    Delete alx-project-0x06 directory

--- alx-project-0x06/pages/generate-text-ai.tsx
+++
@@ -1,3 +0,0 @@
-export default function GenerateText() {
-  return <div className="p-8">Generate Text AI Page</div>;
-}
--- alx-project-0x06/pages/text-to-image.tsx
+++
@@ -1,3 +0,0 @@
-export default function TextToImage() {
-  return <div className="p-8">Text to Image Page</div>;
-}
